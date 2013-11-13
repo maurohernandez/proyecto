51ec0f98eebccd535c54b0bcf6e48e2ce86c7ff6
Mauricio Hernandez <maurohernandez@hispavista.com>
2013-11-12 21:19:39 -0300

    este es otro commit

---
+++ src/OTRA/SurveyBundle/Form/AnswerType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OTRA\SurveyBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AnswerType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('answer')
+            ->add('photo')
+            ->add('video')
+            ->add('question')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'OTRA\SurveyBundle\Entity\Answer'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'otra_surveybundle_answer';
+    }
+}
